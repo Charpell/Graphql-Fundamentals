9b4485b2c8b42233c5632730bd63da9d63dc29e0
Charpell <charpellumeh@gmail.com>
2018-09-20 12:59:59 +0100

    Mutation for Likes

--- Lyrical-GraphQL/client/components/LyricList.js
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
 
 
 class LyricList extends Component {
+  onLike(id) {
+    this.props.mutate({ variables: { id } })
+  }
+  
   renderLyrics() {
     return this.props.lyrics.map(({ id, content }) => {
       return (
         <li key={id} className="collection-item">
           {content}
+          <i
+              className="material-icons"
+              onClick={() => this.onLike(id)}
+            >
+              thumb_up
+            </i>
         </li>
       );
     });
@@ -22,4 +35,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
